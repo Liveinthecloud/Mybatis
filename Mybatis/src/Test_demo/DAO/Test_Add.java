be34f029d549315ba175ec2b242530bf1611bb78
Liveinthecloud <zv279901034@gmail.com>
2019-02-15 23:53:54 +0800

    2019/2/15

--- Mybatis/src/Test_demo/DAO/Test_Add.java
+++ Mybatis/src/Test_demo/DAO/Test_Add.java
@@ -6,7 +6,7 @@ import dao.impl.UserDaoImpl;
 public class Test_Add {
     public static void main(String[] arge){
         UserDaoImpl userDao = new UserDaoImpl();
-        User cat = new User("cat", 50);
+        User cat = new User("dog", 22);
         userDao.addUser(cat);
         System.err.println(cat);
     }
